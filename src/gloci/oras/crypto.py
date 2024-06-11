08fe1f8ab4166f8cff37af4beff2b5b32c9d5344
Vincent Riesop <vincent.riesop@gmail.com>
2024-06-11 15:43:28 +0200

    add annotations from info_yaml to layers

---
+++ src/gloci/oras/crypto.py
@@ -0,0 +1,46 @@
+import hashlib
+
+
+def calculate_sha1(file_path):
+    """Calculate the SHA1 checksum of a file."""
+    sha1_hash = hashlib.sha1()
+    try:
+        with open(file_path, "rb") as f:
+            # Read the file in chunks of 4KB
+            for byte_block in iter(lambda: f.read(4096), b""):
+                sha1_hash.update(byte_block)
+        # Return the hexadecimal digest of the checksum
+        return sha1_hash.hexdigest()
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
+
+
+def calculate_md5(file_path):
+    """Calculate the MD5 checksum of a file."""
+    md5_hash = hashlib.md5()
+    try:
+        with open(file_path, "rb") as f:
+            # Read the file in chunks of 4KB
+            for byte_block in iter(lambda: f.read(4096), b""):
+                md5_hash.update(byte_block)
+        # Return the hexadecimal digest of the checksum
+        return md5_hash.hexdigest()
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
+
+
+def calculate_sha256(file_path):
+    """Calculate the SHA256 checksum of a file."""
+    sha256_hash = hashlib.sha256()
+    try:
+        with open(file_path, "rb") as f:
+            # Read the file in chunks of 4KB
+            for byte_block in iter(lambda: f.read(4096), b""):
+                sha256_hash.update(byte_block)
+        # Return the hexadecimal digest of the checksum
+        return sha256_hash.hexdigest()
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
--- src/gloci/oras/registry.py
+++ src/gloci/oras/registry.py
@@ -8,57 +8,15 @@ import oras.client
 from oras.decorator import ensure_container
 import oras.utils
 import os
-import sys
 import yaml
 import uuid
-import hashlib
 
 from oras.logger import setup_logger, logger
 
+from gloci.oras.crypto import calculate_sha1, calculate_md5, calculate_sha256
+
 setup_logger(quiet=False, debug=True)
 
-
-def calculate_sha1(file_path):
-    """Calculate the SHA1 checksum of a file."""
-    sha1_hash = hashlib.sha1()
-    try:
-        with open(file_path, "rb") as f:
-            # Read the file in chunks of 4KB
-            for byte_block in iter(lambda: f.read(4096), b""):
-                sha1_hash.update(byte_block)
-        # Return the hexadecimal digest of the checksum
-        return sha1_hash.hexdigest()
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-        return None
-
-def calculate_md5(file_path):
-    """Calculate the MD5 checksum of a file."""
-    md5_hash = hashlib.md5()
-    try:
-        with open(file_path, "rb") as f:
-            # Read the file in chunks of 4KB
-            for byte_block in iter(lambda: f.read(4096), b""):
-                md5_hash.update(byte_block)
-        # Return the hexadecimal digest of the checksum
-        return md5_hash.hexdigest()
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-        return None
-
-def calculate_sha256(file_path):
-    """Calculate the SHA256 checksum of a file."""
-    sha256_hash = hashlib.sha256()
-    try:
-        with open(file_path, "rb") as f:
-            # Read the file in chunks of 4KB
-            for byte_block in iter(lambda: f.read(4096), b""):
-                sha256_hash.update(byte_block)
-        # Return the hexadecimal digest of the checksum
-        return sha256_hash.hexdigest()
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-        return None
 
 class Registry(oras.provider.Registry):
     def __init__(self, registry_url, config_path=None):
@@ -139,8 +97,8 @@ class Registry(oras.provider.Registry):
                 "application/vnd.gardenlinux.image.checksum.md5": checksum_md5,
 
             }
-            # if annotations:
-            #    layer["annotations"].update(annotations)
+            if annotations:
+                layer["annotations"].update(annotations)
 
             # update the manifest with the new layer
             manifest["layers"].append(layer)
