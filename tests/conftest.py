b1481f06f3429390e66886e2be90a679485fb611
Vincent Riesop <v.riesop@sap.com>
2024-08-14 07:47:27 +0200

    cleanup test session

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,18 @@
+from os.path import isdir
 from .helper import call_command
+import os
+import shutil
+
 
 def pytest_sessionstart(session):
     call_command("./cert/gencert.sh")
 
+
+def pytest_sessionfinish(session):
+    if os.path.isfile("./cert/oci-sign.crt"):
+        os.remove("./cert/oci-sign.crt")
+    if os.path.isfile("./cert/oci-sign.key"):
+        os.remove("./cert/oci-sign.key")
+
+    if os.path.isdir("./output"):
+        shutil.rmtree("./output")
